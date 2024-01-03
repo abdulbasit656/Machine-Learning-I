0c938741dec946216c30c8d0f3885c2332d7b596
Basit Akram <abdulbasit656@gmail.com>
2024-01-03 20:45:42 +0100

    Add files via upload

---
+++ ML1_Lab01/Naive_Bayes_classifier_smooth.py
@@ -0,0 +1,125 @@
+#############################
+#	BASIT AKRAM
+#	S5161322
+#	ASSIGNMENT 01
+#	MACHINE LEARNING I
+#############################
+
+# Naive Bayes (Laplace Smoothing)
+# 1. Read the data from the csv file
+# 2. Convert categorical data to numerical data
+# 3. Split the data into training and testing data
+# 4. Build a Naive Bayes classifier with Laplace smoothing
+# 5. Train the model
+# 6. Predict and print the output
+
+import pandas as pd
+import numpy as np
+
+# Set dataset path
+PATH = 'weather_dataset.csv'
+# Read the data from the csv file
+df = pd.read_csv(PATH, delim_whitespace=True)
+
+# Set target column
+TARG_COL = df.columns[-1]
+
+# Set test and training proportions
+TRAIN_SIZE = 0.7
+
+# Set smoothing parameter
+ALPHA = 1
+
+# Convert categorical data to numerical data
+def convert_categorical_to_numerical(df):
+    for col in df.columns:
+        class_map = {label: idx for idx, label in enumerate(np.unique(df[col]))}
+        df[col] = df[col].map(class_map)
+
+# Split the data into training and testing data
+def split_data(df, train_size=TRAIN_SIZE, targ_col=TARG_COL):
+    train_X = df.sample(frac=train_size)
+    test_X = df.drop(train_X.index)
+    test_y = test_X[targ_col]
+    # Remove the target column from the testing data
+    test_X = test_X.drop(targ_col, axis=1)
+    return train_X, test_X, test_y
+
+# Check data split
+def check_data_split(train_X, test_X):
+    if test_X.shape[1] != train_X.shape[1] and test_X.shape[1] != train_X.shape[1] - 1:
+        exit('Data split failed')
+
+# Count unique values in each column
+def count_unique_values(df):
+    unique_values = {}
+    for col in df.drop(TARG_COL, axis=1).columns:
+        unique_values[col] = df[col].value_counts().to_dict()
+    return unique_values
+
+# Naive Bayes classifier with Laplace smoothing
+class NaiveBayes:
+    def fit(self, X, unique_values, alpha=ALPHA):
+        self.X = X
+        self.y = self.X[TARG_COL]
+        self.classes = np.unique(self.y)
+        self.cond_prob = {}
+
+        # Remove the target column from the training data
+        self.X = self.X.drop(TARG_COL, axis=1)
+
+        # Calculate the prior probability of the target column
+        self.prior_prob = self.y.value_counts(normalize=True).to_dict()
+        
+        # Calculate the conditional probability of each feature
+        for c in self.classes:
+            self.cond_prob[c] = {}
+            X_c = self.X[self.y==c]
+            for col in X_c.columns:
+                c_counts = X_c[col].value_counts()
+                for k in unique_values[col]:
+                    if k not in c_counts:
+                        c_counts[k] = 0
+                # Smoothing the conditional probability
+                self.cond_prob[c][col] = ((c_counts + alpha) / (X_c[col].value_counts().sum() + alpha*len(unique_values[col]))).to_dict()
+
+    def predict(self, X):
+        posterior = {}
+        y = []
+        # Calculate the posterior probability for each pattern
+        for _, row in X.iterrows():
+            for _, c in enumerate(self.classes):
+                posterior[c] = self.prior_prob[c]
+                for col in row.index:
+                    posterior[c] *= self.cond_prob[c][col][row[col]]
+            y.append(self.classes[np.argmax(list(posterior.values()))])
+        return y
+
+    def error(self, y, y_pred):
+        # Return the error rate
+        return np.sum(y_pred != y) / len(y)
+
+
+convert_categorical_to_numerical(df)
+train_X, test_X, test_y = split_data(df)
+check_data_split(train_X, test_X)
+
+model = NaiveBayes()
+# Train the model
+model.fit(train_X, count_unique_values(df))
+# Predict the output
+y_pred = model.predict(test_X)
+
+# Print the output
+if (len(np.unique(df[TARG_COL])) == 1):
+    print('True output: ', ["yes" for _ in test_y.values])
+    print('Predicted output: ', ["yes" for _ in y_pred])
+elif (len(np.unique(df[TARG_COL])) == 2):
+    print('True output: ', ["yes" if val else "no" for val in test_y.values])
+    print('Predicted output: ', ["yes" if val else "no" for val in y_pred])
+else:
+    print('True output: ', [_ for _ in test_y.values])
+    print('Predicted output: ', y_pred)
+
+# Calculate and print the accuracy of the model
+print(f'Error: {int(model.error(test_y, y_pred) * 100)}%')
---
+++ ML1_Lab03/kNN.py
@@ -0,0 +1,161 @@
+#############################
+#	BASIT AKRAM
+#	S5161322
+#	ASSIGNMENT 03
+#	MACHINE LEARNING I
+#############################
+
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.datasets import mnist
+
+### Task 1 ###
+
+# Load mnist dataset
+(train_X, train_y), (test_X, test_y) = mnist.load_data()
+
+
+### Task 2 ###
+
+# Function for Euclidean distance
+def euclidean_distance(x1, x2):
+   # return np.sqrt(np.sum((x1 - x2)**2))
+   return np.linalg.norm(x1 - x2)
+
+# Function to check the data
+def check_data(train_X, test_X, k):
+   # Check if the number of columns in the training and test sets are the same
+   if train_X.shape[1] != test_X.shape[1]:
+      raise ValueError("The number of columns in the training and test sets is different!")
+   # Check if k is in the correct range
+   if k < 1 or k > train_X.shape[0]:
+      raise ValueError("k is not in the correct range!")
+
+# Function for k-Nearst Neighbors algorithm
+def kNN(train_X, train_y, test_X, k, test_y=None):
+   check_data(train_X, test_X, k)
+
+   # Calculate the distances between the test images and the training images
+   distances = np.zeros((test_X.shape[0], train_X.shape[0]))
+   for i in range(test_X.shape[0]):
+      for j in range(train_X.shape[0]):
+         distances[i, j] = euclidean_distance(test_X[i], train_X[j])
+   
+   # Find the k nearest neighbors
+   k_neighbors = np.zeros((test_X.shape[0], k))
+   for i in range(test_X.shape[0]):
+      k_neighbors[i] = np.argsort(distances[i])[:k]
+
+   # Find the most common class in the k nearest neighbors
+   y_pred = np.zeros(test_X.shape[0])
+   for i in range(test_X.shape[0]):
+      y_pred[i] = np.argmax(np.bincount(train_y[k_neighbors[i].astype(int)]))
+
+   # Compute and return the error rate and the predicted labels if test_y is given
+   # otherwise return only the predicted labels
+   if test_y is not None:
+      return y_pred, np.sum(y_pred != test_y) / test_X.shape[0]
+   else:
+      return y_pred.astype(int)
+
+
+# Select a random subset (5%) of the training data to reduce the computation time
+idx = np.random.permutation(train_X.shape[0])[:int(round(test_X.shape[0] * 0.05))]
+train_X = train_X[idx]
+train_y = train_y[idx]
+
+# Classify 5% of the test data with k=5 (test_y is not given)
+idx = np.random.permutation(test_X.shape[0])[:int(round(test_X.shape[0] * 0.05))]
+
+# prediction = kNN(train_X, train_y, test_X[idx], 5)
+# print("Error rate: ", np.sum(prediction != test_y[idx]) / len(prediction))
+
+# Classify 5% of the test data with k=5 (test_y is given)
+# _, error_rate = kNN(train_X, train_y, test_X[idx], 5, test_y[idx])
+# print("Error rate: ", error_rate)
+
+
+### Task 3 & Addendum ###
+
+# Create a function to calculate the confusion matrix
+def confusion_matrix(y_true, y_pred):
+   # Create a confusion matrix
+   cm = np.zeros((2, 2))
+
+   for i in range(len(y_true)):
+      cm[1][int(y_true[i] == y_pred[i])] += 1
+
+   return cm
+
+classes = np.unique(test_y)
+k = [1, 2, 3, 4, 5, 10, 15, 20, 30, 40, 50]
+accuracies = []
+# 2x2 confusion matrix for each class and each k
+confusion_matrices = np.zeros((len(classes), len(k), 2, 2))
+
+# Predict each digit from the test set for every k and calculate the accuracy
+for i in range(len(classes)):
+   test_X_i = test_X[test_y == i]
+   test_y_i = test_y[test_y == i]
+   # Select a random 10% of test data to reduce the computation time
+   idx = np.random.permutation(test_X_i.shape[0])[:int(round(test_X_i.shape[0] * 0.1))]
+   test_X_i = test_X_i[idx]
+   test_y_i = test_y_i[idx]
+
+   accuracies.append([])
+   for j in range(len(k)):
+      y_pred, err_rate = kNN(train_X, train_y, test_X_i, k[j], test_y_i)
+      accuracies[i].append((1 - err_rate))
+      confusion_matrices[i][j] = confusion_matrix(test_y_i, y_pred)
+
+   # Plot the error rate for each digit and each k
+   plt.plot(k, accuracies[i], label=f'Digit {i}')
+   # Print the progress
+   print("Progress: ", i + 1, "/ 10")
+
+plt.xlabel('k')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
+
+# Calculate sensitivity and F1 score for each digit and each k
+# Note: specificity cannot be calculated and the precision is always 1
+sensitivity = np.zeros((len(classes), len(k)))
+f1_score = np.zeros((len(classes), len(k)))
+for i in range(len(classes)):
+   for j in range(len(k)):
+      sensitivity[i][j] = confusion_matrices[i][j][1][1] / (confusion_matrices[i][j][1][1] + confusion_matrices[i][j][1][0])
+      f1_score[i][j] = 2 * sensitivity[i][j] / (sensitivity[i][j] + 1)
+
+# Plot the sensitivity and F1 score for each digit and each k
+for i in range(len(classes)):
+   plt.plot(k, sensitivity[i], label=f'Digit {i}')
+plt.xlabel('k')
+plt.ylabel('Sensitivity')
+plt.legend()
+plt.show()
+
+for i in range(len(classes)):
+   plt.plot(k, f1_score[i], label=f'Digit {i}')
+plt.xlabel('k')
+plt.ylabel('F1 score')
+plt.legend()
+plt.show()
+
+# Calculate the average sensitivity and F1 score for each digit
+avg_sensitivity = np.zeros(len(classes))
+avg_f1_score = np.zeros(len(classes))
+for i in range(len(classes)):
+   avg_sensitivity[i] = np.mean(sensitivity[i])
+   avg_f1_score[i] = np.mean(f1_score[i])
+
+# Plot the average sensitivity and F1 score for each digit in a subplot
+fig, (ax1, ax2) = plt.subplots(1, 2)
+fig.suptitle('Average sensitivity and F1 score for each digit')
+ax1.bar(classes, avg_sensitivity)
+ax1.set_xlabel('Digit')
+ax1.set_ylabel('Average sensitivity')
+ax2.bar(classes, avg_f1_score)
+ax2.set_xlabel('Digit')
+ax2.set_ylabel('Average F1 score')
+plt.show()
